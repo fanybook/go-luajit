2b207f999f902572196b8f8ace6539c449e4ea77
xingheliufang <xingheliufang@gmail.com>
2022-05-28 23:27:44 +0800

    write example code

---
+++ luajit/bindings.go
@@ -0,0 +1,492 @@
+package luajit
+
+/*
+#cgo pkg-config: luajit
+#include <lua.h>
+#include <luajit.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <luaconf.h>
+#include <stdlib.h>
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+const (
+	LUA_OK        = C.LUA_OK
+	LUA_YIELD     = C.LUA_YIELD
+	LUA_ERRRUN    = C.LUA_ERRRUN
+	LUA_ERRSYNTAX = C.LUA_ERRSYNTAX
+	LUA_ERRMEM    = C.LUA_ERRMEM
+	LUA_ERRERR    = C.LUA_ERRERR
+)
+
+const (
+	LUA_TNONE = C.LUA_TNONE
+
+	LUA_TNIL           = C.LUA_TNIL
+	LUA_TBOOLEAN       = C.LUA_TBOOLEAN
+	LUA_TLIGHTUSERDATA = C.LUA_TLIGHTUSERDATA
+	LUA_TNUMBER        = C.LUA_TNUMBER
+	LUA_TSTRING        = C.LUA_TSTRING
+	LUA_TTABLE         = C.LUA_TTABLE
+	LUA_TFUNCTION      = C.LUA_TFUNCTION
+	LUA_TUSERDATA      = C.LUA_TUSERDATA
+	LUA_TTHREAD        = C.LUA_TTHREAD
+)
+
+const (
+	LUA_REGISTRYINDEX = C.LUA_REGISTRYINDEX
+	LUA_ENVIRONINDEX  = C.LUA_ENVIRONINDEX
+	LUA_GLOBALSINDEX  = C.LUA_GLOBALSINDEX
+)
+
+const (
+	LUA_MULTRET = C.LUA_MULTRET
+)
+
+type LuaState interface {
+	/*
+		state manipulation
+	*/
+	Close()
+	NewThread() LuaState
+
+	//lua_resetthread
+	//lua_atpanic
+
+	/*
+		basic stack manipulation
+	*/
+	GetTop() int
+	SetTop(int)
+	PushValue(int)
+	Remove(int)
+	//insert
+	Replace(int)
+	//checkstack
+
+	//xmove
+
+	/*
+		access functions (stack -> C)
+	*/
+	//isnumber
+	//isstring
+	//iscfunction
+	Type(int) int
+	//typename
+
+	//equal
+	//rawequal
+	//lessthan
+
+	ToNumber(int) float64
+	ToInteger(int) int64
+	//toboolean
+	//pushlstring
+	//objlen
+	//tocfunction
+	ToUserData(int) unsafe.Pointer
+	//tothread
+	//topointer
+
+	/*
+		push functions (C -> stack)
+	*/
+	//pushnil
+	PushNumber(float64)
+	PushInteger(int64)
+	PushString(string)
+	PushCClosure(unsafe.Pointer, int)
+	//pushboolean
+	PushLightUserData(unsafe.Pointer)
+	//pushthread
+
+	/*
+		get functions (Lua -> stack)
+	*/
+	GetTable(int)
+	GetField(int, string)
+	//rawget
+	//rawgeti
+	CreateTable(int, int)
+	//newuserdata
+	//getmetatable
+	//getfenv
+
+	/*
+		set functions (stack -> Lua)
+	*/
+	SetTable(int)
+	SetField(int, string)
+	RawSet(int)
+	//rawseti
+	SetMetaTable(int)
+	//setfenv
+
+	/*
+		`load' and `call' functions (load and run Lua code)
+	*/
+	Call(int, int)
+	PCall(int, int, int) int
+	//cpcall
+	//load
+	//dump
+
+	/*
+		coroutine functions
+	*/
+	//yield
+	//resume
+	//status
+
+	/*
+		garbage-collection function and options
+	*/
+	//gc
+
+	/*
+		miscellaneous functions
+	*/
+	//error
+	//next
+	//concat
+	//getallocf
+	//setallocf
+	//setexdata
+	//getexdata
+	//luasetexdata2
+	//getexdata2
+
+	/*
+		some useful macros
+	*/
+	Pop(int)
+	NewTable()
+	Register(string, unsafe.Pointer)
+	PushCFunction(unsafe.Pointer)
+	//strlen
+	IsFunction(int) bool
+	IsTable(int) bool
+	//islightuserdata
+	//isnil
+	//isboolean
+	//isthread
+	//isnone
+	//isnoneornil
+	//pushliteral
+	GetGlobal(string)
+	SetGlobal(string)
+	ToString(int) string
+
+	/*
+		compatibility macros and functions
+	*/
+	//open()
+	//getregistry
+	//getgccount
+
+	/* hack */
+	//setlevel
+
+	//lauxlib.h
+
+	//l_openlib
+	//l_register
+	//l_getmetafield
+	//l_callmeta
+	//l_typerror
+	//l_argerror
+	//l_checklstring
+	//l_optlstirng
+	//l_optnumber
+	//l_checkinteger
+	//l_optinteger
+	//l_checkstack
+	//l_checktype
+	//l_checkany
+	LNewMetaTable(string)
+	//l_checkudata
+	//l_where
+	//l_error
+	//l_checkoption
+
+	/* pre-defined references */
+	//l_ref
+	//l_unref
+
+	LLoadFile(string) int
+	LLoadBuffer([]byte, string) int
+	LLoadString(string) int
+
+	//l_newstate
+	//l_gsub
+	//l_findtable
+
+	/* From Lua 5.2. */
+	//l_fileresult
+	//l_execresult
+	//l_loadfilex
+	//l_loadbufferx
+	//l_traceback
+	//l_setfuncs
+	//l_pushmodule
+	//l_testudata
+	//l_setmetateble
+
+	/*
+		some useful macros
+	*/
+
+	//l_argcheck
+	//l_checkstrig
+	//l_optstring
+	//l_optint
+	//l_chekclong
+	//l_optlong
+	//l_typename
+	LDoFile(string) int
+	LDoString(string) int
+	LGetMetaTable(string)
+	//l_opt
+	//l_newlibtable
+	//l_newlib
+
+	LOpenLibs()
+}
+
+type CFunction func(unsafe.Pointer) C.int
+
+type luaState struct {
+	l *C.struct_lua_State
+}
+
+func UpValueIndex(idx int) int {
+	return LUA_GLOBALSINDEX - idx
+}
+
+func NewState() LuaState {
+	l := C.luaL_newstate()
+	return &luaState{l}
+}
+
+func FromCLuaState(l unsafe.Pointer) LuaState {
+	return &luaState{(*C.struct_lua_State)(l)}
+}
+
+func (l *luaState) NewThread() LuaState {
+	t := C.lua_newthread(l.l)
+
+	return &luaState{l: t}
+}
+
+func (l *luaState) Close() {
+	C.lua_close(l.l)
+	l.l = nil
+}
+
+func (l *luaState) GetTop() int {
+	return int(C.lua_gettop(l.l))
+}
+
+func (l *luaState) SetTop(idx int) {
+	C.lua_settop(l.l, C.int(idx))
+}
+
+func (l *luaState) PushValue(idx int) {
+	C.lua_pushvalue(l.l, C.int(idx))
+}
+
+func (l *luaState) Remove(idx int) {
+	C.lua_remove(l.l, C.int(idx))
+}
+
+func (l *luaState) Replace(idx int) {
+	C.lua_replace(l.l, C.int(idx))
+}
+
+func (l *luaState) LOpenLibs() {
+	C.luaL_openlibs(l.l)
+}
+
+func (l *luaState) LNewMetaTable(tname string) {
+	tnameCStr := C.CString(tname)
+	defer C.free(unsafe.Pointer(tnameCStr))
+
+	C.luaL_newmetatable(l.l, tnameCStr)
+}
+
+func (l *luaState) LLoadFile(filename string) int {
+	fileNameCStr := C.CString(filename)
+	defer C.free(unsafe.Pointer(fileNameCStr))
+
+	return int(C.luaL_loadfile(l.l, fileNameCStr))
+}
+
+func (l *luaState) LLoadBuffer(buff []byte, name string) int {
+	buffCChar := (*C.char)(C.CBytes(buff))
+	nameCStr := C.CString(name)
+	defer C.free(unsafe.Pointer(nameCStr))
+
+	return int(C.luaL_loadbuffer(l.l, buffCChar, C.ulong(len(buff)), nameCStr))
+}
+
+func (l *luaState) LLoadString(s string) int {
+	sCStr := C.CString(s)
+	defer C.free(unsafe.Pointer(sCStr))
+
+	return int(C.luaL_loadstring(l.l, sCStr))
+}
+
+func (l *luaState) Call(nargs, nresults int) {
+	C.lua_call(l.l, C.int(nargs), C.int(nresults))
+}
+
+func (l *luaState) PCall(nargs, nresults, errfunc int) int {
+	res := C.lua_pcall(l.l, C.int(nargs), C.int(nresults), C.int(errfunc))
+	return int(res)
+}
+
+func (l *luaState) ToNumber(idx int) float64 {
+	return float64(C.lua_tonumber(l.l, C.int(idx)))
+}
+
+func (l *luaState) ToInteger(idx int) int64 {
+	return int64(C.lua_tointeger(l.l, C.int(idx)))
+}
+
+func (l *luaState) ToString(idx int) string {
+	var len C.size_t
+	cStr := C.lua_tolstring(l.l, -1, &len)
+	return C.GoStringN(cStr, C.int(len))
+}
+
+func (l *luaState) ToUserData(idx int) unsafe.Pointer {
+	return C.lua_touserdata(l.l, C.int(idx))
+}
+
+func (l *luaState) GetTable(idx int) {
+	C.lua_gettable(l.l, C.int(idx))
+}
+
+func (l *luaState) GetGlobal(k string) {
+	l.GetField(LUA_GLOBALSINDEX, k)
+}
+
+func (l *luaState) SetGlobal(k string) {
+	l.SetField(LUA_GLOBALSINDEX, k)
+}
+
+func (l *luaState) GetField(idx int, k string) {
+	kCStr := C.CString(k)
+	defer C.free(unsafe.Pointer(kCStr))
+
+	C.lua_getfield(l.l, C.int(idx), kCStr)
+}
+
+func (l *luaState) CreateTable(narr int, nrec int) {
+	C.lua_createtable(l.l, C.int(narr), C.int(nrec))
+}
+
+func (l *luaState) SetTable(idx int) {
+	C.lua_settable(l.l, C.int(idx))
+}
+
+func (l *luaState) SetField(idx int, k string) {
+	kCStr := C.CString(k)
+	defer C.free(unsafe.Pointer(kCStr))
+
+	C.lua_setfield(l.l, C.int(idx), kCStr)
+}
+
+func (l *luaState) RawSet(idx int) {
+	C.lua_rawset(l.l, C.int(idx))
+}
+
+func (l *luaState) SetMetaTable(idx int) {
+	C.lua_setmetatable(l.l, C.int(idx))
+}
+
+func (l *luaState) LDoFile(filename string) int {
+	if ret := l.LLoadFile(filename); ret != LUA_OK {
+		return ret
+	} else {
+		if r := l.PCall(0, LUA_MULTRET, 0); r != LUA_OK {
+			return r
+		} else {
+			return LUA_OK
+		}
+	}
+}
+
+func (l *luaState) LDoString(s string) int {
+	if ret := l.LLoadString(s); ret != LUA_OK {
+		return ret
+	} else {
+		if r := l.PCall(0, LUA_MULTRET, 0); r != LUA_OK {
+			return r
+		} else {
+			return LUA_OK
+		}
+	}
+}
+
+func (l *luaState) LGetMetaTable(s string) {
+	l.GetField(LUA_REGISTRYINDEX, s)
+}
+
+func (l *luaState) Type(idx int) int {
+	return int(C.lua_type(l.l, C.int(idx)))
+}
+
+func (l *luaState) IsFunction(idx int) bool {
+	return l.Type(idx) == LUA_TFUNCTION
+}
+
+func (l *luaState) IsTable(idx int) bool {
+	return l.Type(idx) == LUA_TTABLE
+}
+
+func (l *luaState) PushNumber(n float64) {
+	C.lua_pushnumber(l.l, C.double(n))
+}
+
+func (l *luaState) PushInteger(n int64) {
+	C.lua_pushinteger(l.l, C.long(n))
+}
+
+func (l *luaState) PushString(s string) {
+	cStr := C.CString(s)
+	defer C.free(unsafe.Pointer(cStr))
+
+	C.lua_pushstring(l.l, cStr)
+}
+
+func (l *luaState) PushLightUserData(p unsafe.Pointer) {
+	C.lua_pushlightuserdata(l.l, p)
+}
+
+func (l *luaState) Pop(idx int) {
+	l.SetTop(-idx - 1)
+}
+
+func (l *luaState) NewTable() {
+	l.CreateTable(0, 0)
+}
+
+func (l *luaState) Register(n string, fn unsafe.Pointer) {
+	l.PushCFunction(fn)
+	l.SetGlobal(n)
+}
+
+func (l *luaState) PushCFunction(fn unsafe.Pointer) {
+	l.PushCClosure(fn, 0)
+}
+
+func (l *luaState) PushCClosure(fn unsafe.Pointer, n int) {
+	C.lua_pushcclosure(l.l, (C.lua_CFunction)(fn), C.int(n))
+}
--- main.go
+++ main.go
@@ -1,52 +1,220 @@
 package main
 
-/*
-#cgo pkg-config: luajit
-#include <luajit.h>
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
-#include <luaconf.h>
-#include <stdlib.h>
-*/
-import "C"
-
 import (
 	"fmt"
 	"unsafe"
+
+	"github.com/xingheliufang/go-luajit/luajit"
 )
 
-const luaScript = `
-local ffi = require "ffi"
-ffi.cdef[[
-	extern void print();
-]]
+/*
+typedef struct lua_State lua_State;
 
-print("hello from lua")
-ffi.C.print()
+extern int Move(lua_State*);
+extern int Add(lua_State*);
+extern int ToString(lua_State*);
+extern int NewPointer(lua_State*);
+
+extern int Count(lua_State*);
+extern int NewCounter(lua_State*);
+*/
+import "C"
+
+func Tables() {
+	l := luajit.NewState()
+	defer l.Close()
+
+	const LUA_SCRIPT = `
+		x = { dave = "busy", ian = "idle" }
+	`
+
+	l.LDoString(LUA_SCRIPT)
+
+	l.GetGlobal("x")
+	if l.IsTable(-1) {
+		fmt.Println("is table!")
+	}
+
+	l.PushString("dave")
+	l.GetTable(-2)
+	fmt.Println(l.ToString(-1))
+
+	l.GetGlobal("x")
+	l.GetField(-1, "ian")
+	fmt.Println(l.ToString(-1))
+
+	l.GetGlobal("x")
+	l.PushString("sleeping")
+	l.SetField(-2, "john")
+
+	l.GetGlobal("x")
+	l.GetField(-1, "john")
+	fmt.Println(l.ToString(-1))
+
+}
+
+type Pointer struct {
+	x int64
+	y int64
+}
+
+func (p *Pointer) Move(x, y int64) {
+	p.x += x
+	p.y += y
+}
+
+func (p Pointer) Add(o Pointer) Pointer {
+	return Pointer{
+		x: p.x + o.x,
+		y: p.y + o.y,
+	}
+}
+
+func (p Pointer) ToString() string {
+	return fmt.Sprintf("x = %d, y = %d", p.x, p.y)
+}
+
+//export Move
+func Move(ptr *C.struct_lua_State) C.int {
+	l := luajit.FromCLuaState(unsafe.Pointer(ptr))
+
+	p := (*Pointer)(l.ToUserData(1))
+	x := l.ToInteger(2)
+	y := l.ToInteger(3)
+
+	p.Move(x, y)
+
+	return 0
+}
+
+//export NewPointer
+func NewPointer(ptr *C.struct_lua_State) C.int {
+	l := luajit.FromCLuaState(unsafe.Pointer(ptr))
+
+	x := l.ToInteger(1)
+	y := l.ToInteger(2)
+
+	l.PushLightUserData(unsafe.Pointer(&Pointer{x, y}))
+	l.LGetMetaTable("PointerMetaTable")
+	l.SetMetaTable(-2)
+
+	return 1
+}
+
+//export Add
+func Add(ptr *C.struct_lua_State) C.int {
+	l := luajit.FromCLuaState(unsafe.Pointer(ptr))
+
+	p1 := (*Pointer)(l.ToUserData(1))
+	p2 := (*Pointer)(l.ToUserData(2))
+
+	p3 := p1.Add(*p2)
+
+	l.PushLightUserData(unsafe.Pointer(&p3))
+
+	return 1
+}
+
+//export ToString
+func ToString(ptr *C.struct_lua_State) C.int {
+	l := luajit.FromCLuaState(unsafe.Pointer(ptr))
+
+	p := (*Pointer)(l.ToUserData(1))
+
+	l.PushString(p.ToString())
+
+	return 1
+}
+
+//export Count
+func Count(ptr *C.struct_lua_State) C.int {
+	l := luajit.FromCLuaState(unsafe.Pointer(ptr))
+
+	c := l.ToInteger(luajit.UpValueIndex(1))
+	l.PushInteger(c + 1)
+	l.PushValue(-1)
+
+	l.Replace(luajit.UpValueIndex(1))
+
+	return 1
+}
+
+//export NewCounter
+func NewCounter(ptr *C.struct_lua_State) C.int {
+	l := luajit.FromCLuaState(unsafe.Pointer(ptr))
+
+	l.PushInteger(0)
+
+	l.PushCClosure(unsafe.Pointer(C.Count), 1)
+	return 1
+}
+
+func MetaTable() {
+	l := luajit.NewState()
+	defer l.Close()
+
+	l.LOpenLibs()
+
+	const LUA_SCRIPT = `
+		x = Pointer.new(1, 2)
+		y = Pointer.new(3, 4)
+		z = x + y
+		z:move(1, 1)
+		print(z)
+	`
+
+	l.NewTable()
+	l.PushCFunction(unsafe.Pointer(C.NewPointer))
+	l.SetField(-2, "new")
+	l.SetGlobal("Pointer")
+
+	l.LNewMetaTable("PointerMetaTable")
+
+	l.PushCFunction(unsafe.Pointer(C.Add))
+	l.SetField(-2, "__add")
+	l.PushCFunction(unsafe.Pointer(C.ToString))
+	l.SetField(-2, "__tostring")
+
+	l.PushValue(-1)
+	l.SetField(-2, "__index")
+
+	l.PushCFunction(unsafe.Pointer(C.Move))
+	l.SetField(-2, "move")
+
+	if l.LDoString(LUA_SCRIPT) != luajit.LUA_OK {
+		fmt.Println(l.ToString(-1))
+	}
+}
+
+func Closure() {
+	l := luajit.NewState()
+	defer l.Close()
+
+	l.LOpenLibs()
+
+	const LUA_SCRIPT = `
+	c1 = NewCounter()
+	c2 = NewCounter()
+
+	print("c1: " .. c1())
+	print("c1: " .. c1())
+	print("c1: " .. c1())
+	print("c1: " .. c1())
+
+	print("c2: " .. c2())
 `
 
-//export print
-func print() {
-	fmt.Println("hello from go")
+	l.Register("NewCounter", C.NewCounter)
+
+	if l.LDoString(LUA_SCRIPT) != luajit.LUA_OK {
+		fmt.Println(l.ToString(-1))
+	}
 }
 
 func main() {
-	L := C.luaL_newstate()
-	defer C.lua_close(L)
+	Tables()
 
-	C.luaL_openlibs(L)
+	MetaTable()
 
-	luaScriptCStr := C.CString(luaScript)
-	defer C.free(unsafe.Pointer(luaScriptCStr))
-
-	luaScriptName := C.CString("print-test")
-	defer C.free(unsafe.Pointer(luaScriptName))
-
-	C.luaL_loadbuffer(L, luaScriptCStr, C.ulong(len(luaScript)), luaScriptName)
-
-	result := C.lua_pcall(L, 0, -1, 0)
-	if result != 0 {
-		panic("lua error")
-	}
+	Closure()
 }
